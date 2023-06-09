f874f2ab5737f2ee8f8111a67f1cd2de7876507a
Ahmad Anwar <ahmadanwar20184@gmail.com>
2023-06-09 11:57:51 +0500

    More Products Added
    Styling and more products added

--- SemesterPrj/public/javascripts/landingpage.js
+++ SemesterPrj/public/javascripts/landingpage.js
@@ -2,3 +2,15 @@ function addtocart(){
 //    var cart = document.getElementsByClassName("button1")
    alert("Add to Cart Successfully!");
 }
+
+$(document).ready(function() {
+   $(".img1").mousemove(function(e) {
+     var width = $(this).width();
+     var height = $(this).height();
+     var xPos = e.pageX - $(this).offset().left - width / 2;
+     var yPos = e.pageY - $(this).offset().top - height / 2;
+     var angle = Math.atan2(yPos, xPos) * (180 / Math.PI);
+     $(this).css("transform", "rotateZ(" + angle + "deg)");
+   });
+ });
+ 
